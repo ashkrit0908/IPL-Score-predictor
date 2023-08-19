44b6e276c5f5291168f1af317b08c1dfd1062348
ashkrit0908 <60595434+ashkrit0908@users.noreply.github.com>
2023-08-19 11:28:25 +0530

    Update rfMINE.py

--- rfMINE.py
+++ rfMINE.py
@@ -1,14 +1,16 @@
-//importing library
-import pandas as pd
-//df is our data base
-df = pd.read_csv('xx.csv')
-//defining inputs and target 
-inputs = df.drop('runs',axis = 'columns')
-target = df['runs']
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+
+df = pd.read_csv('xx.csv')
+ 
+inputs = df.drop('runs',axis = 'columns')
+target = df['runs']
+
+
 //model fitting
 model = RandomForestClassifier()
 model.fit(inputs,target)
+
 //output of model based on various input turned into numerals
 print(model.predict([[2,2,6,4,0,5]]))
